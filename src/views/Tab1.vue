6b35116fc829a6b70085d0331e510f34147842af
Sasha <gulenkos@gmail.com>
2021-01-16 14:18:43 +0200

    First commit

--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -1,28 +1,63 @@
 <template>
   <ion-page>
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
+    <ion-button @click="openMenu()">Open menu</ion-button>
+    <ion-menu side="start" menu-id="first" content-id="main">
+      <ion-header>
+        <ion-toolbar color="primary">
+          <ion-title>Actions</ion-title>
         </ion-toolbar>
       </ion-header>
-    
-      <ExploreContainer name="Tab 1 page" />
-    </ion-content>
+      <ion-content id="main">
+        <!--This click works-->
+        <ion-button @click="doAction()">Do action</ion-button>
+        <ion-list>
+          <!--This click does not work-->
+          <ion-item button @click="doAction()">
+            <ion-label>Do action</ion-label>
+          </ion-item>
+        </ion-list>
+      </ion-content>
+    </ion-menu>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+  import {
+    IonContent,
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonMenu,
+    IonList,
+    IonItem,
+    IonLabel,
+    IonButton,
+    menuController
+  } from '@ionic/vue';
+  import { defineComponent } from 'vue';
 
-export default  {
-  name: 'Tab1',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
-}
+  export default defineComponent({
+    name: 'Tab1',
+    components: {
+      IonContent,
+      IonPage,
+      IonHeader,
+      IonToolbar,
+      IonTitle,
+      IonMenu,
+      IonList,
+      IonItem,
+      IonLabel,
+      IonButton
+    },
+    methods: {
+      doAction () {
+        console.log("called");
+      },
+      openMenu () {
+        menuController.open('first');
+      },
+    }
+  })
 </script>
